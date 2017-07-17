97a72a36f97fef4e55fd6332a10f4fe9fa6648ac
DanFerrer <danielferrer212@gmail.com>
2017-07-17 08:34:54 -0400

    :tada: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       },
+       {
+       	test: /\.js$/,
+     		exclude: /(node_modules)/,
+      	use: [
+       		'babel-loader'
+      	]
+       }
+     ]
+   }
+};
